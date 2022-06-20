73f02e631ef21712d15bc7411bf31bf40a30b4b5
eaarroyavave <eaarroyave@softcaribbean.com.co>
2022-06-20 10:32:52 -0500

    5. Add Navigation

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -11,19 +11,9 @@ import { MessageService } from '../../services/message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) { }
-
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  heroes : Hero[] = [];
-  selectedHero?:Hero;
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -31,12 +21,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero):void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
-
 }
